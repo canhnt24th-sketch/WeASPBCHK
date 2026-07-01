d86e6a182e4c3839198d7e4d985470d9780b64d9
NGÔ TUẤN CẢNH <canhnt.24th@daihoclongan.edu.vn>
2026-07-01 09:12:17 +0700

    Lab

--- webdulich/webdulich/Areas/Admin/Models/GroupViewModel.cs
+++ webdulich/webdulich/Areas/Admin/Models/GroupViewModel.cs
@@ -2,8 +2,8 @@
 {
     public class GroupViewModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
